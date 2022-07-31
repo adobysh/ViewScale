ff68445ba4087f85939d6b4d982674f6b0a607e8
Andrei Dobysh <andrei.dobysh@icloud.com>
2022-07-31 19:49:24 +0200

    Added test cases

---
+++ ViewScale/ViewScale.swift
@@ -0,0 +1,22 @@
+//
+//  ViewScale.swift
+//  ViewScale
+//
+//  Created by Andrew on 31.07.22.
+//
+
+import Foundation
+
+public final class ViewScale {
+
+  let name = "ViewScale"
+  
+  public func add(a: Int, b: Int) -> Int {
+    return a + b
+  }
+  
+  public func sub(a: Int, b: Int) -> Int {
+    return a - b
+  }
+    
+}
