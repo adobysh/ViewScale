746fd29d44cd28c76d79ae15d6cde5e6f8e01c8a
Andrei Dobysh <andrei.dobysh@icloud.com>
2022-08-02 21:10:07 +0200

    changes from master

---
+++ ViewScale/ViewScale.h
@@ -0,0 +1,18 @@
+//
+//  ViewScale.h
+//  ViewScale
+//
+//  Created by Andrew on 31.07.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViewScale.
+FOUNDATION_EXPORT double ViewScaleVersionNumber;
+
+//! Project version string for ViewScale.
+FOUNDATION_EXPORT const unsigned char ViewScaleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewScale/PublicHeader.h>
+
+
